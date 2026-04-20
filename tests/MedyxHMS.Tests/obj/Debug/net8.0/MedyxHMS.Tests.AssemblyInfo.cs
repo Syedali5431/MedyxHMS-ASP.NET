a8f1cf279fdccdc6690a251df6de621cb98ea014
Ali Nasir <alin@nws.com.au>
2026-04-21 07:46:35 +1000

    Fixing

--- tests/MedyxHMS.Tests/obj/Debug/net8.0/MedyxHMS.Tests.AssemblyInfo.cs
+++ tests/MedyxHMS.Tests/obj/Debug/net8.0/MedyxHMS.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedyxHMS.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94abdc59b5a7264158e2106bd536cd6dbb2d1fb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fe334753f1955b3a635b32fb4ab0439caa5bee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedyxHMS.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedyxHMS.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
